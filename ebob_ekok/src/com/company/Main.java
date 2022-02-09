3a83c7242a7822dc0cb20e834e3d7b0e2b10f5bc
zuleyhaeker <60626948+zuleyhaeker@users.noreply.github.com>
2022-02-10 02:24:12 +0300

    Add files via upload

---
+++ ebob_ekok/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("n1 sayısını giriniz");
+        int n1 = input.nextInt();
+        System.out.print("n2 sayısını giriniz");
+        int n2 = input.nextInt();
+        int ebob = 1;
+
+        int i=1;
+        if (n1<n2){
+            while(i<n1){
+                if(n1%i==0 && n2%i==0){
+                    ebob =i;
+                }
+                i++;
+            }
+        }
+        else{
+            while(i<n2){
+                if(n1%i==0 && n2%i==0){
+                    ebob=i;
+                }
+                i++;
+            }
+        }
+        System.out.println("ebob : "+ ebob );
+
+        int ekok=(n1*n2)/ebob;
+        System.out.println("ekok : "+ ekok);
+        
+    }
+}
+
